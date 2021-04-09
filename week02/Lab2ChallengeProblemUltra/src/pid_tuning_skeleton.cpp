ae8a8da4961b3ce4a699f620cbca3746afb444bc
hushmandesmaeili <81536889+hushmandesmaeili@users.noreply.github.com>
2021-04-09 16:37:17 -0400

    Updated PID skeleton for Ultrasonic

--- week02/Lab2ChallengeProblemUltra/src/pid_tuning_skeleton.cpp
+++ week02/Lab2ChallengeProblemUltra/src/pid_tuning_skeleton.cpp
@@ -18,11 +18,22 @@ PIDController rightMotorController(12, 1, 0, 300); //start with  Kp = 1
 volatile uint8_t PIDController::readyToPID = 0; //a flag that is set when the PID timer overflows
 
 PIDController wallFollow(1, 0.1, 1, 75); //wall follow controller
-ourTimer wallFollowTimer(50); //every 50ms
+ourTimer wallFollowTimer(100); //every 100ms
 
-const uint8_t sharpRead = 18;
-const float VREF = 5.0;
-float lastSharpSamples[5];
+volatile uint16_t pulseStart = 0;
+volatile uint16_t pulseEnd = 0;
+
+//define the states for the echo capture
+enum PULSE_STATE {PLS_IDLE, PLS_WAITING_LOW, PLS_WAITING_HIGH, PLS_CAPTURED};
+
+//and initialize to IDLE
+volatile PULSE_STATE pulseState = PLS_IDLE;
+
+//this may be most any pin, connect the pin to Trig on the sensor
+const uint8_t trigPin = 14;
+
+// const float VREF = 5.0;
+float lastSamples[5];
 uint16_t sampleCount = 0;
 
 uint16_t targetDistance = 25;
@@ -39,7 +50,16 @@ void setup()
 {
   Serial.begin(115200);
   //while(!Serial) {}  //IF YOU DON'T COMMENT THIS OUT, YOU MUST OPEN THE SERIAL MONITOR TO START
-  Serial.println("Hi");
+  // Serial.println("Hi");
+
+  noInterrupts(); //disable interupts while we mess with the control registers
+
+  sampleCount = 0;
+  
+  //sets timer 3 to normal mode (16-bit, fast counter)
+  TCCR3A = 0; 
+  
+  interrupts(); //re-enable interrupts
 
   noInterrupts(); //disable interupts while we mess with the Timer4 registers
   
@@ -61,31 +81,53 @@ void setup()
 
   // pinMode(6, OUTPUT); //COMMENT THIS OUT TO SHUT UP THE PIEZO!!!
 
-  pinMode(sharpRead, INPUT); //read sharp IR
+  pinMode(trigPin, OUTPUT);
+  pinMode(13, INPUT); //explicitly make 13 an input
 
 }
 
 float targetLeft = 15; //max speed is around 75
 float targetRight = 15;
 
-float getDistance(){
-  uint16_t adc_out = analogRead(sharpRead);
-  float voltage_out = ((float) adc_out * VREF) / 1023;
+/*
+ * Commands the ultrasonic to take a reading
+ */
+void CommandPing(int trigPin)
+{
+  cli(); //disable interrupts
 
-  float distance = 15.1 / (voltage_out - 0.333);
+  TIFR3 = 0x20; //clear any interrupt flag that might be there
 
-  return distance;
+  TIMSK3 |= 0x20; //enable the input capture interrupt
+  TCCR3B |= 0xC0; //set to capture the rising edge on pin 13; enable noise cancel
+
+  sei(); //re-enable interrupts
+
+  //update the state and command a ping
+  pulseState = PLS_WAITING_LOW;
+  
+  digitalWrite(trigPin, HIGH); //command a ping by bringing TRIG HIGH
+  delayMicroseconds(10);      //we'll allow a delay here for convenience; it's only 10 us
+  digitalWrite(trigPin, LOW);  //must bring the TRIG pin back LOW to get it to send a ping
 }
 
-float rollingAverage(float arr[5]){
+/*
+ * Returns the rolling average of array passed as argument
+ */
+float rollingAverage(float arr[5]) 
+{
   float avg = 0;
   float sum = 0;
-
+  
   int i;
-  for (i = 0; i < 5; i++) sum += arr[i];
+  for (i = 0; i < 5; i++)
+    sum += arr[i];
 
-  if (sampleCount > 5) avg = sum / 5;
-  else avg = sum / sampleCount;
+  //if condition to check if first 5 samples to adjust denominator in average calculation
+  if (sampleCount > 5)
+    avg = sum / 5;
+  else
+    avg = sum / sampleCount;
 
   return avg;
 }
@@ -100,11 +142,46 @@ void loop()
     // targetRight = targetRight < 40 ? 50 : 25;
   }
   
-  if(wallFollowTimer.isExpired()){ //for using sharp IR
+  if(wallFollowTimer.isExpired() && pulseState == PLS_IDLE) { //for using Ultrasonic
+
+    CommandPing(trigPin); //command a ping
+  }
+
+  if(pulseState == PLS_CAPTURED) //we got an echo
+  {
+
+    //increment sample count
     sampleCount++;
-    lastSharpSamples[sampleCount % 5] = getDistance();
-    float avg_distance = rollingAverage(lastSharpSamples);
-    float error = avg_distance - targetDistance;
+
+    //update the state to IDLE
+    pulseState = PLS_IDLE;
+
+    /*
+     * Calculate the length of the pulse (in timer counts!). Note that we turn off
+     * interrupts for a VERY short period so that there is no risk of the ISR changing
+     * pulseEnd or pulseStart. The way the state machine works, this wouldn't 
+     * really be a problem, but best practice is to ensure that no side effects can occur.
+     */
+    noInterrupts();
+    uint16_t pulseLengthTimerCounts = pulseEnd - pulseStart;
+    interrupts();
+    
+    //EDIT THIS LINE: convert pulseLengthTimerCounts, which is in timer counts, to time, in us
+    //You'll need the clock frequency and the pre-scaler to convert timer counts to time
+    
+    uint32_t pulseLengthUS = (pulseLengthTimerCounts * 4); //pulse length in us
+
+
+    //EDIT THIS LINE AFTER YOU CALIBRATE THE SENSOR: put your formula in for converting us -> cm
+    float distancePulse = pulseLengthUS / 58.0;    //distance in cm
+    
+    //store distancePulse to lastSamples array, using circular indexing for last five samples
+    lastSamples[sampleCount % 5] = distancePulse;
+
+    // float avg_distance = rollingAverage(lastSamples);
+    
+    // float error = avg_distance - targetDistance;
+    float error = distancePulse - targetDistance;
 
     turnEffort = wallFollow.ComputeEffort(error);
     targetLeft =  targetSpeed - turnEffort;
@@ -140,20 +217,7 @@ void loop()
     int16_t errorRight = targetRight - speedRight; //calculate the error
     float effortRight = rightMotorController.ComputeEffort(errorRight); //calculate effort from error
     
-    // motors.setEfforts(effortLeft, 0); 
     motors.setEfforts(effortLeft, effortRight); 
-
-    // Serial.print(millis());
-    // Serial.print('\t');
-    // Serial.print(targetLeft);
-    // Serial.print('\t');
-    // Serial.print(speedLeft);
-    // Serial.print('\t');
-    // Serial.print(effortLeft/10.0); //divide effort by 10 for better plotting
-
-    // //you may want to add more serial printout here for testing
-
-    // Serial.print('\n');
   }
 
   /* for reading in gain settings
@@ -175,4 +239,26 @@ ISR(TIMER4_OVF_vect)
   countsRight = encoders.getCountsRight();
 
   PIDController::readyToPID = 1;
+}
+
+/*
+ * ISR for input capture on pin 13. We can precisely capture the value of TIMER3
+ * by setting TCCR3B to capture either a rising or falling edge. This ISR
+ * then reads the captured value (stored in ICR3) and copies it to the appropriate
+ * variable.
+ */
+ISR(TIMER3_CAPT_vect)
+{
+  if(pulseState == PLS_WAITING_LOW) //we're waiting for a rising edge
+  {
+    pulseStart = ICR3; //copy the input capture register (timer count)
+    TCCR3B &= 0xBF;    //now set to capture falling edge on pin 13
+    pulseState = PLS_WAITING_HIGH;
+  }
+
+  else if(pulseState == PLS_WAITING_HIGH) //waiting for the falling edge
+  {
+    pulseEnd = ICR3;
+    pulseState = PLS_CAPTURED; //raise a flag to indicate that we have data
+  }
 }
