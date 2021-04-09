521784fc5cbb1f5c637f5d90166f7abb24c7f00d
jhooney <73201726+jhooney@users.noreply.github.com>
2021-04-09 16:34:57 -0400

    State Machine Lab 1
    State machine added

--- week02/Lab2ChallengeProblem/src/pid_tuning_skeleton.cpp
+++ week02/Lab2ChallengeProblem/src/pid_tuning_skeleton.cpp
@@ -6,12 +6,16 @@
  */
 
 #include <Romi32U4.h>
+#include <stdio.h>
+#include <Arduino.h>
 
 #include "serial_comm.h"
 #include "PIDcontroller.h"
 #include "ourTimer.h"
+#include "reflectance.h"
 
 Romi32U4ButtonA buttonA;
+reflectance lineSensor;
 
 PIDController leftMotorController(12, 1, 0, 300); //start with  Kp = 1
 PIDController rightMotorController(12, 1, 0, 300); //start with  Kp = 1
@@ -62,7 +66,7 @@ void setup()
   // pinMode(6, OUTPUT); //COMMENT THIS OUT TO SHUT UP THE PIEZO!!!
 
   pinMode(sharpRead, INPUT); //read sharp IR
-
+  lineSensor.setup();
 }
 
 float targetLeft = 15; //max speed is around 75
@@ -90,78 +94,108 @@ float rollingAverage(float arr[5]){
   return avg;
 }
 
+typedef enum {
+  IDLE,
+  WAITING,
+  WALLING
+} State;
+State state = IDLE;
+State lastState = IDLE;
+
+uint32_t waitStart = 0;
+
 void loop() 
-{    
-  if(buttonA.getSingleDebouncedPress())
-  {
-    // targetLeft += 15;
-    // if(targetLeft > 300) targetLeft = 300;
-    // // targetLeft = targetLeft < 40 ? 50 : 25;
-    // targetRight = targetRight < 40 ? 50 : 25;
-  }
-  
-  if(wallFollowTimer.isExpired()){ //for using sharp IR
-    sampleCount++;
-    lastSharpSamples[sampleCount % 5] = getDistance();
-    float avg_distance = rollingAverage(lastSharpSamples);
-    float error = avg_distance - targetDistance;
+{ 
+  switch(state){
+    case IDLE:
+      motors.setEfforts(0, 0);
+      if(buttonA.getSingleDebouncedPress()){
+        if(lastState == IDLE){
+          state = WAITING;
+          lastState = IDLE;
+          waitStart = millis();
+        } else {
+          state = WALLING;
+          lastState = IDLE;
+        }
+      }
+    break;
+    case WAITING:
+      if(millis() - waitStart >= 1000){
+        state = WALLING;
+        lastState = IDLE;
+      }
+    break;
+    case WALLING:
+      if(lineSensor.both()){
+        state = IDLE;
+        lastState = WALLING;
+        break;
+      }
+      if(wallFollowTimer.isExpired()){ //for using sharp IR
+        sampleCount++;
+        lastSharpSamples[sampleCount % 5] = getDistance();
+        float avg_distance = rollingAverage(lastSharpSamples);
+        float error = avg_distance - targetDistance;
 
-    turnEffort = wallFollow.ComputeEffort(error);
-    targetLeft =  targetSpeed - turnEffort;
-    targetRight = targetSpeed + turnEffort;
-  }
+        turnEffort = wallFollow.ComputeEffort(error);
+        targetLeft =  targetSpeed - turnEffort;
+        targetRight = targetSpeed + turnEffort;
+      }
 
-  if(PIDController::readyToPID) //timer flag set
-  {
-    // reset the flag
-    PIDController::readyToPID = 0;
-    
-    // for tracking previous counts
-    static int16_t prevLeft = 0;
-    static int16_t prevRight = 0;
+      if(PIDController::readyToPID) //timer flag set
+      {
+        // reset the flag
+        PIDController::readyToPID = 0;
+        
+        // for tracking previous counts
+        static int16_t prevLeft = 0;
+        static int16_t prevRight = 0;
 
-    /*
-     * Do PID stuffs here. Note that we turn off interupts while we read countsLeft/Right
-     * so that it won't get accidentally updated (in the ISR) while we're reading it.
-     */
-    noInterrupts();
+        /*
+        * Do PID stuffs here. Note that we turn off interupts while we read countsLeft/Right
+        * so that it won't get accidentally updated (in the ISR) while we're reading it.
+        */
+        noInterrupts();
 
-    int16_t speedLeft = countsLeft - prevLeft;
-    prevLeft = countsLeft;
+        int16_t speedLeft = countsLeft - prevLeft;
+        prevLeft = countsLeft;
 
-    int16_t speedRight = countsRight - prevRight;
-    prevRight = countsRight;
+        int16_t speedRight = countsRight - prevRight;
+        prevRight = countsRight;
 
-    interrupts();
+        interrupts();
 
-    int16_t errorLeft = targetLeft - speedLeft; //calculate the error
-    float effortLeft = leftMotorController.ComputeEffort(errorLeft); //calculate effort from error
+        int16_t errorLeft = targetLeft - speedLeft; //calculate the error
+        float effortLeft = leftMotorController.ComputeEffort(errorLeft); //calculate effort from error
 
-    int16_t errorRight = targetRight - speedRight; //calculate the error
-    float effortRight = rightMotorController.ComputeEffort(errorRight); //calculate effort from error
-    
-    // motors.setEfforts(effortLeft, 0); 
-    motors.setEfforts(effortLeft, effortRight); 
+        int16_t errorRight = targetRight - speedRight; //calculate the error
+        float effortRight = rightMotorController.ComputeEffort(errorRight); //calculate effort from error
+        
+        // motors.setEfforts(effortLeft, 0); 
+        motors.setEfforts(effortLeft, effortRight); 
 
-    // Serial.print(millis());
-    // Serial.print('\t');
-    // Serial.print(targetLeft);
-    // Serial.print('\t');
-    // Serial.print(speedLeft);
-    // Serial.print('\t');
-    // Serial.print(effortLeft/10.0); //divide effort by 10 for better plotting
+        // Serial.print(millis());
+        // Serial.print('\t');
+        // Serial.print(targetLeft);
+        // Serial.print('\t');
+        // Serial.print(speedLeft);
+        // Serial.print('\t');
+        // Serial.print(effortLeft/10.0); //divide effort by 10 for better plotting
 
-    // //you may want to add more serial printout here for testing
+        // //you may want to add more serial printout here for testing
 
-    // Serial.print('\n');
-  }
+        // Serial.print('\n');
+      }
 
-  /* for reading in gain settings
-   * CheckSerialInput() returns true when it gets a complete string, which is
-   * denoted by a newline character ('\n'). Be sure to set your Serial Monitor to 
-   * append a newline
-   */
-  if(CheckSerialInput()) {ParseSerialInput();}
+      /* for reading in gain settings
+      * CheckSerialInput() returns true when it gets a complete string, which is
+      * denoted by a newline character ('\n'). Be sure to set your Serial Monitor to 
+      * append a newline
+      */
+      if(CheckSerialInput()) {ParseSerialInput();}
+        break;
+      }
 }
 
 /*
