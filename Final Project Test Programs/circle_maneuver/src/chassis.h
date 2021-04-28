08aac2741db6f18a3b2254aab1c147e6f16c159f
hushmandesmaeili <81536889+hushmandesmaeili@users.noreply.github.com>
2021-04-27 22:31:22 -0400

    Created program to test circle maneuvering
    Created a folder for all testing programs that can be used for final project.
    
    Created a cicle_maneuver test program. Uses inverse kinematics to update pose and set the target positions as coordinates of a circle.

---
+++ Final Project Test Programs/circle_maneuver/src/chassis.h
@@ -0,0 +1,62 @@
+#ifndef __CHASSIS_H
+#define __CHASSIS_H
+
+#include <Romi32U4.h>
+#include "PIDcontroller.h"
+
+class Chassis
+{
+private:
+    //parameters -- these will need to be updated after you do your experiments
+    float wheel_track = 15.2; //cm
+    float wheel_diam = 7.085; //cm
+    float ticks_per_rotation = 1440; // from the datasheet
+
+    // constants to control speed of the wheels for inverse kinematics
+    float kpD = 0.75;
+    float kpTheta = 12;
+
+    //current Pose
+    float x = 50;
+    float y = 0;
+    float theta = PI / 2;
+
+    //
+    const int BUFFER = 2;
+
+    //current target
+    float x_target = 0;
+    float y_target = 0;
+    float th_target = 0; //may or may not be specified
+
+    //target wheels speeds
+    float targetSpeedLeft = 0;
+    float targetSpeedRight = 0;
+
+    //for calculating speeds
+    int16_t prevEncLeft = 0;
+    int16_t prevEncRight = 0;
+
+    //wheel speed controllers
+    PIDController leftMotorController; 
+    PIDController rightMotorController; 
+
+    //actual speed
+    int16_t speedLeft = 0;
+    int16_t speedRight = 0;
+
+    //be sure to set this -- it needs to match your "readyToPID" period
+    uint32_t timestepMS = 16; // in ms
+
+public:
+    Chassis(void);
+    void Init(void);
+    void SetTargetSpeeds(float left, float right) { targetSpeedLeft = left; targetSpeedRight = right; }
+    void SetTargetPosition(float xt, float yt) { x_target = xt; y_target = yt; }
+    void UpdatePose(void);
+    void UpdateSpeeds(void);
+    void MoveToPoint(void);
+    bool AreWeThere(void);
+};
+
+#endif
---
+++ Final Project Test Programs/circle_maneuver/src/go-to-position.cpp
@@ -0,0 +1,116 @@
+/*
+ * Code for using TCC4 for precision PID timing.
+ * You'll need to set TOP to set the interval
+ * 
+ * This code adds the ability to tune the gains and change the targets
+ */
+
+#include <Romi32U4.h>
+#include "chassis.h"
+#include "Timer.h"
+
+//const uint8_t IR_PIN = A6;
+
+Romi32U4ButtonA buttonA;
+Romi32U4ButtonB buttonB;
+Romi32U4ButtonC buttonC;
+Chassis chassis;
+
+Timer waitTimer(1000);
+
+const float radius = 50;  // in cm
+const float angleInt = PI / 8;  // angle interval
+int count = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  //while(!Serial) {}  //IF YOU DON'T COMMENT THIS OUT, YOU MUST OPEN THE SERIAL MONITOR TO START
+  Serial.println("Hi");
+
+  chassis.Init();
+}
+
+enum STATE {DR_IDLE, DR_WAITING, DR_TARGET, DR_DRIVING};
+STATE state = DR_IDLE;
+
+enum DESTINATION {DEST_NONE, DEST_A, DEST_B, DEST_C};
+DESTINATION destination = DEST_NONE;
+
+void HandleButton(DESTINATION d)
+{
+  Serial.println(d);
+  destination = d;
+  waitTimer.reset(1000);
+
+  state = DR_WAITING;
+}
+
+void HandleTimerExpired(void)
+{
+    switch(state)
+    {
+      case DR_WAITING:
+        if(waitTimer.isExpired()){
+          // if(destination == DEST_A) //drive straight
+          // {
+            // chassis.SetTargetSpeeds(12.2, 12.2);  //YOU'LL WANT TO CHANGE THESE
+            // waitTimer.reset(5000);          //YOU'LL WANT TO CHANGE THESE
+            // chassis.SetTargetPosition(30.0, 30.0);
+
+            state = DR_TARGET;
+          // }
+        //   else if(destination == DEST_B) //spin in place
+        //   {
+        //   //   chassis.SetTargetSpeeds(-12.2, 12.2);  //YOU'LL WANT TO CHANGE THESE
+        //   //   waitTimer.reset(4000);          //YOU'LL WANT TO CHANGE THESE
+
+        //     chassis.SetTargetPosition(60.0, 0.0);
+        //     state = DR_DRIVING;
+        //   }
+        //   else if(destination == DEST_C) //curl
+        //   {
+        //   //   chassis.SetTargetSpeeds(8.8, 13.2);  //YOU'LL WANT TO CHANGE THESE
+        //   //   waitTimer.reset(5000);          //YOU'LL WANT TO CHANGE THESE
+
+        //     chassis.SetTargetPosition(30.0, -30.0);
+        //     state = DR_DRIVING;
+        //   }
+        }
+      break;
+      case DR_TARGET:
+        float x = radius * cos(angleInt * count);
+        float y = radius * sin(angleInt * count);
+        chassis.SetTargetPosition(x, y);
+      case DR_DRIVING:
+        chassis.MoveToPoint();
+        if(chassis.AreWeThere()) {
+          count++;
+          state = DR_TARGET;
+        }
+      break;;                                                                                                                                                                                  
+      default:
+        chassis.SetTargetSpeeds(0, 0);
+        state = DR_IDLE;
+        destination = DEST_NONE;
+    }
+}
+
+void loop() 
+{
+  if(buttonA.getSingleDebouncedPress()) HandleButton(DEST_A);
+  if(buttonB.getSingleDebouncedPress()) HandleButton(DEST_B);
+  if(buttonC.getSingleDebouncedPress()) HandleButton(DEST_C);
+
+  HandleTimerExpired();
+
+  if(PIDController::readyToPID) //timer flag set
+  {
+    // reset the flag
+    PIDController::readyToPID = 0;
+    chassis.UpdateSpeeds();  
+    chassis.UpdatePose();
+  }
+
+  // if(waitTimer.isExpired()) HandleTimerExpired();
+}
